1e95449c1412812f076d29ae8ec021bfa6e50f65
s4hil <s4hilp4rr4y@gmail.com>
2024-01-13 02:35:51 +0530

    register ui

--- js/config.js
+++ js/config.js
@@ -8,3 +8,18 @@ var GET_SUBMISSIONS_URL = BASE_URL+"getData.php";
 var DEL_SUBMISSION_URL = BASE_URL+"delSubmission.php";
 var GET_ALL_PARTICIPANTS = "http://localhost/code-arena-api/getAllParticipants.php";
 var GET_ALL_TEAMS = "http://localhost/code-arena-api/getAllTeams.php";
+
+
+var team_sizes = [
+	{ "name": "Code Arena", "size": 4 },
+	{ "name": "Idea X", "size": 3 },
+	{ "name": "CodifyFix", "size": 2 },
+	{ "name": "Trivia Quizzix", "size": 3 },
+	{ "name": "Startup Expo", "size": 4 },
+	{ "name": "PerSpecta", "size": 3 },
+	{ "name": "Gaming", "size": 4 },
+	{ "name": "Artify", "size": 1 },
+	{ "name": "Vision Xpo", "size": 4 },
+	{ "name": "Robo Race", "size": 3 },
+	{ "name": "Trading", "size": 1 }
+]
